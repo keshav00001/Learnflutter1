6fc8b955e200feaca009c8f218bc84583a497869
kishan <Keshavking111@gmail.com>
2021-08-08 01:50:08 +0530

    day5 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,38 @@
 //import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:myapp1/pages/login_page.dart';
+import 'package:myapp1/utils/routes.dart';
+
 //import 'dart:developer';
-import 'home_page.dart';
+import 'pages/home_page.dart';
+//import 'pages/login_page.dart';
+
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({key}) : super(key: key);
-
-  // @override
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage()
+      },
     );
   }
 }
